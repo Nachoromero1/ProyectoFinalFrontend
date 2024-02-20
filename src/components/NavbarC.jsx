6a4d8d21155b2d32757ec7f0d9b4932f8d7e140b
unknown <romeronacho888@gmail.com>
2024-02-20 13:41:32 -0300

    agregue un contador a los iconos de carrito y favoritos, tambien hice un carrousell con 3 imagenes

--- src/components/NavbarC.jsx
+++ src/components/NavbarC.jsx
@@ -4,6 +4,9 @@ import { FaHeart, FaShoppingCart, FaQuestionCircle } from 'react-icons/fa';
 import RegisterPage from '../pages/RegisterPage';
 
 const NavBar = () => {
+  // Simulación de la cantidad de favoritos
+  const favoritosCount = 10;
+
   return (
     <Navbar sticky="top" bg="light" expand="lg">
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -14,24 +17,23 @@ const NavBar = () => {
         <Nav>
           <Nav.Link href="">Iniciar sesión</Nav.Link>
           <Nav.Link href="/register">Registrarse</Nav.Link>
-
         </Nav>
         <Navbar.Brand href="/" className="mx-auto">
           <img src="https://i.pinimg.com/originals/18/41/15/1841153bcf0a942c568d29d3a78e454d.png" alt="Logo" width="25px" />
         </Navbar.Brand>
-        <Nav className="ml-auto">
+        <Nav>
           <Nav.Link href="#carrito">
             <FaShoppingCart />  <Badge bg="secondary">5</Badge>
           </Nav.Link>
         </Nav>
         <Nav>
-          <Nav.Link href="#ayuda">
-            <FaQuestionCircle />
+          <Nav.Link href="#favoritos">
+            <FaHeart /> {favoritosCount > 0 && <Badge bg="secondary">{favoritosCount}</Badge>}
           </Nav.Link>
         </Nav>
-        <Nav>
-          <Nav.Link href="#favoritos">
-            <FaHeart />
+        <Nav className="ml-auto">
+          <Nav.Link href="#ayuda">
+            <FaQuestionCircle />
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
 import agregarProductosLocalStorage from "../Helps/ArrayPrducts"
 import CardsC from "../components/CardsC"
 import CarouselC from "../components/CarouselC"
@@ -8,19 +10,41 @@ const HomePage = () => {
   const arrayProductos = JSON.parse(localStorage.getItem ('products')) || [];
   const urlImageHomePage = "https://i.ytimg.com/vi/xaHsm-8vE7w/maxresdefault.jpg"
 
-
   return (
     <>
-    <CarouselC urlImage={urlImageHomePage } ancho={'100%'} />
-    <div className="container mt-5">
-      <div className="row">
-        {
-          arrayProductos.map((producto) => 
-          <CardsC producto={producto} key={producto.id}/>)
-        }
-        
+      <div className="carousel-container">
+        <Carousel className="w-100 h-100">
+          <Carousel.Item>
+            <img
+              className="d-block w-100 h-100"
+              src="https://images.squarespace-cdn.com/content/v1/5b63290096d455957b1edc85/1533231470811-03P7MORBPE6OOYFWVUYH/GS-HERO.png?format=2500w"
+              alt="First slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100 h-100"
+              src="https://www.attitude.co.uk/wp-content/uploads/sites/5/2023/06/Gymshark.jpg"
+              alt="Second slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100 h-100"
+              src="https://www.reasonwhy.es/media/library/gymshark-david-laid.jpg"
+              alt="Third slide"
+            />
+          </Carousel.Item>
+        </Carousel>
       </div>
-    </div>
+      <div className="container mt-5">
+        <div className="row">
+          {
+            arrayProductos.map((producto) => 
+            <CardsC producto={producto} key={producto.id}/>)
+          }
+        </div>
+      </div>
     </>
   )
 }
