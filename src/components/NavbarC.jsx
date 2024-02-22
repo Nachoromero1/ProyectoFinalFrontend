b341ddf0c01115cd3f9900c62b4b56aa5adbadbd
unknown <romeronacho888@gmail.com>
2024-02-22 16:22:39 -0300

    Navbar arreglo

--- src/components/NavbarC.jsx
+++ src/components/NavbarC.jsx
@@ -1,41 +1,27 @@
 import React from 'react';
-import { Navbar, Nav, Badge } from 'react-bootstrap';
-import { FaHeart, FaShoppingCart, FaQuestionCircle } from 'react-icons/fa';
-import RegisterPage from '../pages/RegisterPage';
+import { Navbar, Nav } from 'react-bootstrap';
 
 const NavBar = () => {
-  // Simulación de la cantidad de favoritos
-  const favoritosCount = 10;
-
   return (
     <Navbar sticky="top" bg="light" expand="lg">
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
+        <div style={{ marginLeft: '20px' }}> {/* Agregar margen a la izquierda */}
+          <Navbar.Brand href="/" className="color-nav navbar navbar-expand-lg navbar-light ">
+            <img className="navbar-logo" src="https://i.pinimg.com/originals/18/41/15/1841153bcf0a942c568d29d3a78e454d.png" alt="Logo" width="25px" />
+          </Navbar.Brand>
+        </div>
         <Nav className="mr-auto">
-          <Nav.Link href="/">Inicio</Nav.Link>
+          <Nav.Link href="/" style={{ fontSize: '18px' }}>Inicio</Nav.Link>
+          <Nav.Link href="/about" style={{ fontSize: '18px' }}>Sobre mí</Nav.Link>
+          <Nav.Link href="/contact" style={{ fontSize: '18px' }}>Contacto</Nav.Link>
         </Nav>
-        <Nav>
-          <Nav.Link href="">Iniciar sesión</Nav.Link>
-          <Nav.Link href="/register">Registrarse</Nav.Link>
-        </Nav>
-        <Navbar.Brand href="/" className="mx-auto">
-        <img className="navbar-logo" src="https://i.pinimg.com/originals/18/41/15/1841153bcf0a942c568d29d3a78e454d.png" alt="Logo" width="25px" />
-        </Navbar.Brand>
-        <Nav>
-          <Nav.Link href="#carrito">
-            <FaShoppingCart />  <Badge bg="secondary">5</Badge>
-          </Nav.Link>
-        </Nav>
-        <Nav>
-          <Nav.Link href="#favoritos">
-            <FaHeart /> {favoritosCount > 0 && <Badge bg="secondary">{favoritosCount}</Badge>}
-          </Nav.Link>
-        </Nav>
-        <Nav className="ml-auto">
-          <Nav.Link href="#ayuda">
-            <FaQuestionCircle />
-          </Nav.Link>
-        </Nav>
+        <div style={{ marginLeft: 'auto' }}>
+          <Nav>
+            <Nav.Link href="/" style={{ fontSize: '18px' }}>Iniciar sesión</Nav.Link>
+            <Nav.Link href="/register" style={{ fontSize: '18px' }}>Registrarse</Nav.Link>
+          </Nav>
+        </div>
       </Navbar.Collapse>
     </Navbar>
   );
